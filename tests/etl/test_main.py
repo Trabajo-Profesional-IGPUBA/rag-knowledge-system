0bb304da5e0748ff9d991af3b1d9c53644dc2719
s-g-u <surbano@fi.uba.ar>
2026-09-10 01:44:45 -0300

    docs: documentar sobre CAs de Configuración de la concurrencia que cubre este archivo

--- tests/etl/test_main.py
+++ tests/etl/test_main.py
@@ -4,7 +4,7 @@ from main import DEFAULT_MAX_WORKERS, _get_max_workers
 
 """
 Cubren "Paralelizar la ingesta de documentos (ETL)":
-  - Configuración de la concurrencia-> CA-1.1 a CA-1.5
+  - Configuración de la concurrencia-> CA-1.1 a CA-1.6
 """
 
 
