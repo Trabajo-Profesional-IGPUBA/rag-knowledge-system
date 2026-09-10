26cc5617715494b4ac3bf0e62bd5d7ac8e45bd84
s-g-u <surbano@fi.uba.ar>
2026-09-10 01:59:47 -0300

    docs: documentar sobre CAs de Procesamiento de archivos que cubre este archivo

--- tests/etl/test_main.py
+++ tests/etl/test_main.py
@@ -5,6 +5,7 @@ from main import DEFAULT_MAX_WORKERS, _get_max_workers
 """
 Cubren "Paralelizar la ingesta de documentos (ETL)":
   - Configuración de la concurrencia-> CA-1.1 a CA-1.6
+  - Procesamiento de archivos-> CA-2.3
   """
 
 
